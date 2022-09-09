9b0c2dd0734bda3a74dbf9f23085e1c8d75b6265
Philipp Eder <philipp.eder@greenbone.net>
2022-09-09 13:23:18 +0200

    Add: notus advisories and products for container image
    To use the notus data within this image you need to copy
    `/usr/local/src/notus/advisories`
    `/usr/local/src/notus/products`
    
    to `/var/lib/notus/`.
    
    The products definition declare each installed package as vulnerable.

---
+++ slackware/create_products.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+PRODUCT_NAME=$(cat version.txt)
+OID=$(grep oid advisories/slackware.notus | sed 's/.*"\(\([0-9]\+\.\)\+\)\([0-9]\+\).*/\1\3/')
+LST=$(cat packages.lst)
+printf '{ "version": "1.0", "package_type": "slack", "product_name": "'
+printf "$PRODUCT_NAME"
+printf '", "advisories": [ { "oid": "'
+printf "$OID"
+printf '", "fixed_packages": ['
+for pkg in $LST; do
+  printf '{ "name": null, "full_version": null, "full_name": "'
+  printf "$pkg"
+  printf '", "specifier": ">"},'
+done
+printf '{ "name": null, "full_version": null, "full_name": null, "specifier": ">="}'
+printf ']}]}\n'
